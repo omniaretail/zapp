46d1cfd18e76e39b6fcbbb9bbb091ad0e304f546
Wichard Riezebos <wichardriezebos@gmail.com>
2017-01-31 14:30:33 +0100

    added package-service

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Zapp is a Windows process orchestrator for packed apps.
 | --- | --- |
 | **Build** | [![Build status](https://ci.appveyor.com/api/projects/status/sv453plywnnulnf9?svg=true)](https://ci.appveyor.com/project/OmniaRetail/zapp) |
 | **NuGet** | [![NuGet](https://buildstats.info/nuget/Zapp)](https://www.nuget.org/packages/Zapp/) |
-| **Gitter** |not yet |
+| **Gitter** | not yet |
 
 ## Installation
 
@@ -23,9 +23,11 @@ The code below is an example how to use the library.
 ```
 using Zapp;
 using Zapp.Rest;
+using Zapp.Packages;
 
 var server = new ZappServer(
     new OwinRestService("http://localhost:6464/"),	// Instance of: IRestService
+	new ZipPackageService(),						// Instance of: IPackageService
 );
 
 server.Start();
--- Zapp/ZappServer.cs
+++ Zapp/ZappServer.cs
@@ -1,4 +1,5 @@
-﻿using Zapp.Rest;
+﻿using Zapp.Packages;
+using Zapp.Rest;
 
 namespace Zapp
 {
@@ -8,14 +9,19 @@ namespace Zapp
     public class ZappServer : IZappServer
     {
         private readonly IRestService apiService;
+        private readonly IPackageService packageService;
 
         /// <summary>
         /// Initializes a new <see cref="ZappServer"/> with the specified dependencies.
         /// </summary>
-        /// <param name="apiService">The instance of <see cref="IRestService"/> used for external actions.</param>
-        public ZappServer(IRestService apiService)
+        /// <param name="apiService">The instance of <see cref="IRestService"/> used for web actions.</param>
+        /// <param name="packageService">The instance of <see cref="IPackageService"/> used for package actions.</param>
+        public ZappServer(
+            IRestService apiService,
+            IPackageService packageService)
         {
             this.apiService = apiService;
+            this.packageService = packageService;
         }
 
         /// <summary>
