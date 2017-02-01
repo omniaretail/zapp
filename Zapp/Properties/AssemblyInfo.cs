5312b478638f17573ca393d6e2782b769392240e
Wichard Riezebos <wichardriezebos@gmail.com>
2017-02-01 16:39:22 +0100

    removed semver

--- Zapp/Properties/AssemblyInfo.cs
+++ Zapp/Properties/AssemblyInfo.cs
@@ -26,11 +26,11 @@ using System.Runtime.InteropServices;
 //
 //      Major Version
 //      Minor Version 
-//      Build Number 
+//      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
