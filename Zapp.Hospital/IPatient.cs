9df93318b45e3cc80a4dfb8bafe935744eb5887f
Wichard Riezebos <wichardriezebos@gmail.com>
2017-07-14 16:56:08 +0200

    Added zapp-hospital to as nuget package.

---
+++ Zapp.Hospital/IPatient.cs
@@ -0,0 +1,18 @@
+﻿namespace Zapp.Hospital
+{
+    /// <summary>
+    /// Represents an interface to provide health details about something in a zapp fusion.
+    /// </summary>
+    public interface IPatient
+    {
+        /// <summary>
+        /// The Id of the patient.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Represents if the patient is healthy.
+        /// </summary>
+        bool IsHealthy();
+    }
+}
